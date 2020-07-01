3494165b0eedfe190ee45a69c0113b1046a23540
aw <61329389+yongkisuheno@users.noreply.github.com>
2020-07-02 03:52:39 +0700

    Update function.php

--- function.php
+++ function.php
@@ -11,7 +11,7 @@ $header[] = "X-AppVersion: 3.30.2";
 $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
 $header[] = "Connection: keep-alive";
 $header[] = "X-User-Locale: id_ID";
-$header[] = "X-Location: -6.605275,106.801561";
+$header[] = "X-Location: -6.1827141,106.8214631";
 $header[] = "X-Location-Accuracy: 3.0";
 if ($pin):
 $header[] = "pin: $pin";
